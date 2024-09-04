7fcdce31ddf0f9e18e0455bcd575344cf46618f9
Garrick Aden-Buie <garrick@adenbuie.com>
2024-09-04 13:44:42 -0400

    fix: whitespace

--- pkg-py/src/brand_yaml/_brand_color.py
+++ pkg-py/src/brand_yaml/_brand_color.py
@@ -18,45 +18,45 @@ class BrandColor(BrandWith[str]):
 
     foreground
         The foreground color, used for text.
-    
+
     background
         The background color, used for the page or main background.
-    
+
     primary
         The primary accent color, i.e. the main theme color. Typically used for
         hyperlinks, active states, primary action buttons, etc.
-    
+
     secondary
         The secondary accent color. Typically used for lighter text or disabled
         states.
-    
+
     tertiary
         The tertiary accent color. Typically an even lighter color, used for
         hover states, accents, and wells.
-    
+
     success
         The color used for positive or successful actions and information.
-    
+
     info
         The color used for neutral or informational actions and information.
-    
+
     warning
         The color used for warning or cautionary actions and information.
-    
+
     danger
         The color used for errors, dangerous actions, or negative information.
-    
+
     light
         A bright color, used as a high-contrast foreground color on dark
         elements or low-contrast background color on light elements.
-    
+
     dark
         A dark color, used as a high-contrast foreground color on light elements
         or high-contrast background color on light elements.
-    
+
     emphasis
         A color used to emphasize or highlight text or elements.
-    
+
     link
         The color used for hyperlinks. If not defined, the `primary` color is
         used.
