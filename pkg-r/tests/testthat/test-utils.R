8278771162dc30e2509672cfc0fab58c4420f40d
Garrick Aden-Buie <garrick@adenbuie.com>
2025-09-26 09:45:07 -0400

    fix: kebab -> snake casing with conflicts

--- pkg-r/tests/testthat/test-utils.R
+++ pkg-r/tests/testthat/test-utils.R
@@ -46,17 +46,24 @@ describe("maybe_convert_font_size_to_rem()", {
 
 describe("list_restyle_names()", {
   it("converts names to snake case", {
-    input_list <- list(a_b = 1, c_d = 2)
+    input_list <- list("a-b" = 1, "c-d" = 2)
     expected_output <- list(a_b = 1, c_d = 2)
     output <- list_restyle_names(input_list, style = "snake")
     expect_equal(output, expected_output)
   })
 
   it("converts names to kebab case", {
-    input_list <- list("a-b" = 1, "c-d" = 2)
+    input_list <- list(a_b = 1, c_d = 2)
     expected_output <- list("a-b" = 1, "c-d" = 2)
     output <- list_restyle_names(input_list, style = "kebab")
     expect_equal(output, expected_output)
+  })
+
+  it("does not change names without '-' or '_'", {
+    x <- list(aa = 1, ab = 2)
+    names(x) <- c("aa", "ab")
+    expect_identical(names(list_restyle_names(x, "snake")), c("aa", "ab"))
+    expect_identical(names(list_restyle_names(x, "kebab")), c("aa", "ab"))
   })
 
   it("handles nested lists", {
@@ -78,5 +85,53 @@ describe("list_restyle_names()", {
     expected_output <- list(1, 2, 3)
     output <- list_restyle_names(input_list, style = "snake")
     expect_equal(output, expected_output)
+  })
+
+  it("avoids converting names that would create duplicates", {
+    input_list <- list("a_b" = "a_b", "a-b" = "a-b")
+    expect_equal(list_restyle_names(input_list[2:1], "snake"), input_list[2:1])
+    expect_equal(list_restyle_names(input_list, "kebab"), input_list)
+  })
+
+  it("snake: skips if it would duplicate an existing original name", {
+    x <- list(a_b = 1, "a-b" = 2)
+    expect_identical(list_restyle_names(x, "snake"), x)
+    expect_identical(list_restyle_names(x[2:1], "snake"), x[2:1])
+  })
+
+  it("kebab: skips if it would duplicate an existing original name", {
+    x <- list("a-b" = 1, a_b = 2)
+    expect_identical(list_restyle_names(x, "kebab"), x)
+    expect_identical(list_restyle_names(x[2:1], "kebab"), x[2:1])
+  })
+
+  it("snake: only entirely non-duplicated names can be converted", {
+    x <- list("p-q" = 1, "p-q" = 2, "r-s" = 3)
+    expected <- set_names(x, c("p-q", "p-q", "r_s"))
+    expect_equal(list_restyle_names(x, "snake"), expected)
+  })
+
+  it("kebab: only entirely non-duplicated names can be converted", {
+    x <- list("p_q" = 1, "p_q" = 2, "r_s" = 3)
+    expected <- set_names(x, c("p_q", "p_q", "r-s"))
+    expect_equal(list_restyle_names(x, "kebab"), expected)
+  })
+
+  it("round trips snake -> kebab -> snake", {
+    # note we start with entirely snake-case names
+    x <- list("p_q" = 1, "p_q" = 2, "r_s" = 3)
+    expect_equal(
+      list_restyle_names(list_restyle_names(x, "kebab"), "snake"),
+      x
+    )
+  })
+
+  it("round trips kebab -> snake -> kebab", {
+    # note we start with entirely kebab-case names
+    x <- list("p-q" = 1, "p-q" = 2, "r-s" = 3)
+    expect_equal(
+      list_restyle_names(list_restyle_names(x, "snake"), "kebab"),
+      x
+    )
   })
 })
